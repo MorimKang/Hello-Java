1179521000a2a64c0d501d17fa8e1cfd9a13c3a5
user <user@192.168.1.100>
2019-01-13 17:11:19 +0900

    commit javadev

---
+++ javadev/src/basic/Hello2.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Hello2 {
+	public static void main(String[] args) {
+		System.out.print("Hello Java ");
+		System.out.print("Hello Eclipse ");
+		System.out.print("Hello World");
+	}
+}
---
+++ javadev/src/basic/Optest2.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Optest2 {
+	public static void main(String[] args) {
+		int i = 7;
+		int j = 4;
+		
+		System.out.println(i > j);
+		System.out.println(i >= j);
+		System.out.println(i < j);
+		System.out.println(i <= j);
+		System.out.println(i == j);
+		System.out.println(i != j);
+		
+		
+	}
+}
